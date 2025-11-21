b277ad330484e3d3a0b7385c8dc5a181e0e44bfd
st10451478-Khaya-Nxumalo <ST10451478@vcconnect.edu.za>
2025-11-21 13:54:21 +0200

    Data layer Built

---
+++ CMCS4/CMCS4.Data/Context/CMCS4DbContext.cs
@@ -0,0 +1,15 @@
+﻿using CMCS4.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CMCS4.Data
+{
+    public class CMCS4DbContext : DbContext
+    {
+        public CMCS4DbContext(DbContextOptions<CMCS4DbContext> options)
+            : base(options) { }
+
+        public DbSet<Lecturer> Lecturers { get; set; }
+        public DbSet<Claim> Claims { get; set; }
+    }
+}
---
+++ CMCS4/CMCS4.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,45 @@
+﻿using CMCS4.Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CMCS4.Data.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly CMCS4DbContext _ctx;
+
+        public GenericRepository(CMCS4DbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task<List<T>> GetAllAsync() =>
+            await _ctx.Set<T>().ToListAsync();
+
+        public async Task<T?> GetAsync(int id) =>
+            await _ctx.Set<T>().FindAsync(id);
+
+        public async Task<T> AddAsync(T entity)
+        {
+            _ctx.Set<T>().Add(entity);
+            await _ctx.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _ctx.Set<T>().Update(entity);
+            await _ctx.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetAsync(id);
+            if (entity != null)
+            {
+                _ctx.Set<T>().Remove(entity);
+                await _ctx.SaveChangesAsync();
+            }
+        }
+    }
+}
